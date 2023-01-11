ed26c6ff4cb8c73f8d6c1d0404edc2a94a31d902
Mathieu Mack <mathieu.mack@outlook.fr>
2023-01-11 10:34:36 +0100

    Remove useless file

--- src/CKan.NetClient/Clients/Organizations/HttpModels/OrganizationListResult.cs
+++
@@ -1,8 +0,0 @@
-﻿using CKan.NetClient.Clients.HttpModels;
-
-namespace CKan.NetClient.Clients.Organizations.Models
-{
-    public class OrganizationListResult : HttpCallListResult<string>
-    {
-    }
-}
